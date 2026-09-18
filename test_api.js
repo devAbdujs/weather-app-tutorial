d0919d54381ca2177cef646ee940db0e65fd5789
devAbdujs <abdu12.aau@gmail.com>
2026-09-18 23:37:17 +0300

    fix(deploy): remove playwright from dependencies to fix Vercel serverless limits

--- test_api.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ path: '.env.local' });
-async function test() {
-  const res = await fetch('https://generativelanguage.googleapis.com/v1beta/models?key=' + process.env.gemini_api_key1);
-  const json = await res.json();
-  console.log(json.models.map(m => m.name));
-}
-test();
